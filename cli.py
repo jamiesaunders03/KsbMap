17dc233a4fea5654d8208fd3d4bb97d9d533c083
jamie saunders <jamiesaunders03@gmail.com>
2025-06-19 14:56:41 +0100

    Make CLI nice

--- cli.py
+++ cli.py
@@ -1,3 +1,88 @@
-from src.lookup import mod_code_to_ksbs
+from src.lookup import mod_code_to_ksbs, filter_se_skills, filter_ba_skills
+from src.data.module_code import ModuleCode
 
-print(*mod_code_to_ksbs("DC3IPR"), sep="\n")
+
+def _get_cli_args(args: list[tuple[str, str]]) -> dict[str, str]:
+    cli_args = dict(help=False, list=False, module="", filter="")
+    for opt, arg in optlist:
+        if opt in ("-m", "--module"):
+            cli_args["module"] = arg
+        elif opt in ("-f", "--filter"):
+            cli_args["filter"] = arg
+        elif opt in ("-h", "--help"):
+            cli_args["help"] = True
+        elif opt in ("-l", "--list"):
+            cli_args["list"] = True
+
+    return cli_args
+
+
+def _help():
+    print("INTRO")
+    print("\tWelcome to the Module Skill CLI")
+    print("\tYou can use this to search for the relevant KSBs for each module")
+    print("")
+    print("ARGS")
+    print("\t-h|--help                         The help pages (this)")
+    print("\t-l|--list                         List all known module codes")
+    print("\t-m|--module=    [module code]     The module code to search. If not looking at the help pages or module ")
+    print("\t                                  list then this is a required argument")
+    print("\t-f|--filter=    [filter string]   Optional filter to only show either BA/SE skills, if this starts with ")
+    print("\t                                  a 's', doesn't show BA only skills. If it starts with a 'b', doesn't")
+    print("\t                                  show SE only skills")
+    print("")
+    print("EXAMPLES")
+    print("\tpython3 cli.py -m DC3IPR")
+    print("\t\tGet all skills that should be displayed in the DC3IPR Module")
+    print("")
+    print("\tpython3 cli.py -m DC2TPR --filter=se")
+    print("\t\tGet all skills that a software engineer should demonstrate during the team project module")
+
+
+def _list_all_modules():
+    print("MODULE LIST:")
+    for mod in ModuleCode:
+        print(f"    {mod.name}")
+
+
+def _search(mod_code: str, filter: str):
+    if mod_code == "":
+        raise ValueError("Please specify a module code")
+
+    try:
+        mod = ModuleCode[cli_args["module"].upper()]
+    except KeyError:
+        raise ValueError(f"Unknown module code: {cli_args['module']}")
+
+    filter_func = None
+    if filter.lower().startswith("s"):
+        filter_func = filter_se_skills
+    elif filter.startswith("b"):
+        filter_func = filter_ba_skills
+    elif filter != "":
+        print(f"Unknown filter modifier: {filter}, no filtering taking place")
+
+    skills = mod_code_to_ksbs(mod, filter=filter_func)
+
+    print("Code    | Group                     | Description")
+    print("        |                           |")
+    for skill in skills:
+        print(f"{skill.code:<8}| {skill.group.name:<25} | {skill.description}")
+
+
+if __name__ == '__main__':
+    import getopt
+    import sys
+
+    short_ops = "m:f:hl"
+    long_ops = ["module=", "filter=", "help", "list"]
+
+    optlist, _ = getopt.getopt(sys.argv[1:], short_ops, long_ops)
+    cli_args = _get_cli_args(optlist)
+
+    if len(optlist) == 0 or cli_args["help"]:
+        _help()
+    elif cli_args["list"]:
+        _list_all_modules()
+    else:
+        _search(cli_args["module"], cli_args["filter"])
--- src/lookup.py
+++ src/lookup.py
@@ -1,18 +1,39 @@
 
+from typing import Callable
+
 from .data.module_code import ModuleCode
 from .data.skill import Skill
 from .mapping.lo_ksb_map import skills
 from .mapping.module_lo_map import module_map
 
 
-def mod_code_to_ksbs(mod_code: str) -> list[Skill]:
-    mod = ModuleCode(0)
-    try:
-        mod = ModuleCode[mod_code.upper()]
-    except KeyError:
-        return []
+def mod_code_to_ksbs(mod_code: ModuleCode, filter: Callable[[list[Skill]], list[Skill]] = None) -> list[Skill]:
+    """
+    Gets all the skills associated with a given module code.
+    :param mod_code: The module code to get skills for.
+    :param filter: Optional function - filters items in response
+    :return: A list of all skills associated with a given module code.
+    """
 
-    los = [lo[1] for lo in module_map if lo[0] == mod]
+    los = [lo[1] for lo in module_map if lo[0] == mod_code]
     mod_skills = [s[0] for s in skills if s[1] in los]
 
+    if filter is not None:
+        mod_skills = filter(mod_skills)
+
     return mod_skills
+
+
+def filter_se_skills(skill_list: list[Skill]) -> list[Skill]:
+    """
+    Returns a list of all skills passed in, except for ones that are for Business Analysts
+    """
+    return [s for s in skill_list if not s.group.name.startswith('BA')]
+
+
+def filter_ba_skills(skill_list: list[Skill]) -> list[Skill]:
+    """
+    Returns a list of all skills passed in, except for ones that are for Software Engineers
+    """
+    return [s for s in skill_list if not s.group.name.startswith('SE')]
+
